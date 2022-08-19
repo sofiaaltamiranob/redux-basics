f20032598d36cee2bc881eec4dcff9a05a8dc991
sofialtamiranob <sofiaaaltamiranob@gmail.com>
2022-08-19 12:04:54 -0300

    Add Store devtools

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import * as actions from './contador/contador.actions';
+
+interface AppState{
+  contador: number;
+}
 
 @Component({
   selector: 'app-root',
@@ -6,18 +12,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  contador: number;
+  contador: number = 0;
 
-  constructor(){
-    this.contador = 10;
+  constructor( private store: Store<AppState>){
+    this.store.subscribe( state => {
+      console.log(state);
+      this.contador = state.contador;
+    })
   }
 
   incrementar(){
-    this.contador ++;
+    this.store.dispatch( actions.incrementar());
   }
 
   decrementar(){
-    this.contador --;
+    this.store.dispatch( actions.decrementar());
   }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { environment } from '../environments/environment';
+
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+
+import { contadorReducer } from './contador/contador.reducer';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,7 +20,9 @@ import { NietoComponent } from './contador/nieto/nieto.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot({ contador: contadorReducer }),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
